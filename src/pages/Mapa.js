d7ed21c4eae52567a47cbcc034e54bc7a780903c
Alexandre-Mattos <alexandremattosbc@gmail.com>
2024-07-04 19:58:07 -0300

    [FEATURE] show map

--- src/pages/Mapa.js
+++ src/pages/Mapa.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
 
 export default function Mapa() {
+  const [region, setRegion] = useState({
+    latitude: -23.55052,
+    longitude: -46.633308,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  })
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Mapa</Text>
+      <MapView
+        style={styles.map}
+        region={region}
+        onRegionChangeComplete={setRegion}
+      >
+        <Marker
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude,
+          }}
+          title={'São Paulo'}
+          description={'Descrição do Local'}
+        />
+      </MapView>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
+  map: {
+    position: 'absolute',
   },
-});
+})
